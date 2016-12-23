43eed28b9330a7e6b8bc9430db8aa4feb7d01269
manolis <olakiril@hotmail.com>
2016-12-23 16:20:06 -0600

    New tables

--- Licker.py
+++ Licker.py
@@ -1,10 +1,13 @@
 import pygame
 
+
 class Licker:
     """ this class handles the licks
     """
+    def __init__(self, logger):
+        self.logger = logger
 
-    def lick(self):
+    def licked(self):
         response = False
 
         events = pygame.event.get()
@@ -12,6 +15,7 @@ class Licker:
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
+                    self.logger.log_lick()
                     response = True
 
         return response
