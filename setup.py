b6133455768aa3341c82b715cf51c3e2021767cf
olakiril <olakiril@hotmail.com>
2017-01-24 16:26:54 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+long_description = "State system for mouse behavior"
+
+
+setup(
+    name='pymouse',
+    version='0.1.dev1',
+    description="State system for mouse behavior",
+    long_description=long_description,
+    author='Emmanouil Froudarakis',
+    author_email='froudara@bcm.edu',
+    license="GNU LGPL",
+    url='https://github.com/olakiril/PyMouse',
+    keywords='mouse behavior control',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy', 'sh', 'concurrent', 'pygame', 'imageio'],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: GNU LGPL',
+        'Topic :: Database :: Front-Ends',
+    ]
+)
