60bfa7ff54f8e457eb3651be49f149c0837eebda
manolis <olakiril@hotmail.com>
2017-03-20 10:35:56 -0500

    Calibration with multiple probes & selective storing

--- Database.py
+++ Database.py
@@ -110,10 +110,11 @@ class CalibrationTask(dj.Lookup):
     # Calibration parameters
     task_idx                     : int             # task identification number
     ---
-    probe                        : int            # probe number
-    pulse_dur                    : int            # duration of pulse in ms
-    pulse_num                    : int            # number of pulses
-    pulse_interval               : int            # interval between pulses in ms
+    probe='[1,2]'                : varchar(128)    # probe number
+    pulse_dur                    : int             # duration of pulse in ms
+    pulse_num                    : int             # number of pulses
+    pulse_interval               : int             # interval between pulses in ms
+    save='yes'                   : enum('yes','no')# store calibration
     """
 
 
--- run.py
+++ run.py
@@ -44,25 +44,32 @@ def train(logger=logg):
 def calibrate(logger=logg):
     """ LickSpout calibration runner"""
     task_idx = (SetupInfo() & dict(setup=logger.setup)).fetch1['task_idx']
-    duration, probe, pulsenum, pulse_interval = \
-        (CalibrationTask() & dict(task_idx=task_idx)).fetch1['pulse_dur', 'probe', 'pulse_num', 'pulse_interval']
+    duration, probes, pulsenum, pulse_interval, save = \
+        (CalibrationTask() & dict(task_idx=task_idx)).fetch1['pulse_dur',
+                                                             'probe',
+                                                             'pulse_num',
+                                                             'pulse_interval',
+                                                             'save']
+    probes = eval(probes)
     valve = ValveControl(logger)  # get valve object
     print('Running calibration')
-    trial = 0
-    while trial < pulsenum:
-        print('Pulse %d/%d' % (trial+1, pulsenum), end="\r", flush=True)
-        valve.give_liquid(probe, duration, False)  # release liquid
+    pulse = 0
+    while pulse < pulsenum:
+        print('Pulse %d/%d' % (pulse+1, pulsenum), end="\r", flush=True)
+        for probe in probes:
+            valve.give_liquid(probe, duration, False)  # release liquid
         time.sleep(duration/1000 + pulse_interval/1000)  # wait for next pulse
-        trial += 1  # update trial
+        pulse += 1  # update trial
+    if save == 'yes':
+        for probe in probes:
+            logger.log_pulse_weight(duration, probe, pulsenum)  # insert
     print('Done calibrating')
-    logger.log_pulse_weight(duration, probe, pulsenum)  # insert
 
 
 while not logg.get_setup_state() == 'stopped':
     # wait for remote start
     while logg.get_setup_state() == 'ready':
         time.sleep(1)
-
     # run experiment unless stopped
     if not logg.get_setup_state() == 'stopped':
         eval(logg.get_setup_task())(logg)
