c858c84b1c9afa5e54a1fc951c868abcd24bc196
olakiril <olakiril@hotmail.com>
2017-05-16 14:51:14 -0500

    major analysis code development

--- matlab/+beh/LiquidDelivery.m
+++ matlab/+beh/LiquidDelivery.m
@@ -19,6 +19,31 @@ classdef LiquidDelivery < dj.Relvar
             times = msec2tmst(fetch(beh.Session & obj),fetchn(obj,'time'));
             plot(times,ones(size(times)) * params.factor,'.','color',params.color)
         end
+        
+        function plotDays(obj, varargin)
+            
+            mice = unique(fetchn(obj & 'animal_id>0','animal_id'));
+            for imouse = 1:length(mice)
+                figure
+                daytimeFunc = @(sesstime, times) datenum(sesstime,'YYYY-mm-dd HH:MM:SS') - 18/24 + times/1000/3600/24;
+                k.animal_id = mice(imouse);
+                [times, sess_times, rew] = fetchn(obj * beh.Session & k,...
+                    'time','session_tmst','reward_amount');
+                dtimes = daytimeFunc(sess_times,times);
+                [days, ~, IC]= unique(floor(dtimes),'rows');
+                TotalReward = nan(length(days),1);
+                for iday = 1:length(days)
+                    TotalReward(iday) = sum(rew(IC==iday));
+                end
+                plot(TotalReward/1000)
+                set(gca,'xtick',1:iday,'xticklabel',datestr(days),'XTickLabelRotation',45)
+                hold on
+                plot([1 iday],[1 1],'-.r')
+                ylim([0 3])
+                ylabel('Delivered liquid (ml)')
+                title(sprintf('Animal ID %d',mice(imouse)))
+            end
+        end
      end
     
 end
--- matlab/+beh/Session.m
+++ matlab/+beh/Session.m
@@ -20,15 +20,15 @@ classdef Session < dj.Relvar
     methods
         function plot(obj)
             figure
-            plot(beh.Lick & obj,'color',[0 0 0])
-            hold on
-            plot(beh.Trial & obj,'factor',2,'color',[0 0 0.5])
+            plot(beh.Lick & obj,'color',[0 0 0],'factor',2.01)
+            hold onn
+            plot(beh.Trial & obj,'factor',2,'color',[0 0 1])
             plot(beh.Trial & obj,'factor',1.9,'color',[0.5 0.5 1],'time','end_time')
             plot(beh.LiquidDelivery & obj,'factor',1.5,'color',[0 1 0])
             plot(beh.AirpuffDelivery & obj,'factor',1.4,'color',[1 0 0])
             ylim([-2 8])
             set(gca,'box','off','ycolor',[1 1 1],'ytick',[])
-            l = legend('Licks','Trial Start','Trial End','Liquid','Air');
+            l = legend('Licks','Trial Start','pretrial','Trial End','Liquid','Air');
             set(l,'box','off')
         end
     end
