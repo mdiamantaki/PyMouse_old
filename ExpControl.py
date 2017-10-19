4b4bc8f63271b04d4a1e051e935a3f8144c0401c
Saumil Patel <spatel@bcm.edu>
2017-10-18 19:58:12 -0700

    changed command processing to only respond to changes in command

--- ExpControl.py
+++ ExpControl.py
@@ -11,6 +11,7 @@ class ExpControl:
         self.exprmt = None
         self.logger = logger
         self.logger.setup_experiment_schema()
+        self.prev_command = None
 
     def do_run_trial(self):
         self.exprmt.pre_trial()
@@ -38,47 +39,49 @@ class ExpControl:
 
     def do_start_session(self):
         """start stimulation session"""
-        self.logger.init_params()  # clear settings from previous session
-        self.logger.log_session()  # start session
-        self.logger.update_setup_state('sessionRunning')
+        if not self.logger.get_setup_state() == 'sessionRunning':
+            self.logger.init_params()  # clear settings from previous session
+            self.logger.log_session()  # start session
+            self.logger.update_setup_state('sessionRunning')
 
     def do_start_stim(self):
         """start stimulation trials"""
-        self.params = (Task() & dict(task_idx=self.logger.task_idx)).fetch1()  # get parameters
-        self.timer = Timer()  # main timer for trials
-        self.exprmt = eval(self.params['exp_type'])(self.logger, self.timer, self.params)  # get experiment & init
-        self.exprmt.prepare()  # prepare stuff
-        print(self.exprmt.run())
-        self.logger.update_setup_state('stimRunning')
-        while self.logger.get_setup_state_control() == 'startStim' and self.exprmt.run():
-                print(self.exprmt.run())
+        if not self.logger.get_setup_state() == 'stimRunning':
+            self.params = (Task() & dict(task_idx=self.logger.task_idx)).fetch1()  # get parameters
+            self.timer = Timer()  # main timer for trials
+            self.exprmt = eval(self.params['exp_type'])(self.logger, self.timer, self.params)  # get experiment & init
+            self.exprmt.prepare()  # open stimulus window and prepare the protocol
+            self.logger.update_setup_state('stimRunning')
+            while self.logger.get_setup_state_control() == 'startStim' and self.exprmt.run():
                 self.logger.ping()
                 self.do_run_trial()
-        if not self.exprmt.run():
             self.do_stop_stim()
 
     def do_stop_stim(self):
         # # # # # Cleanup # # # # #
-        if self.logger.get_setup_state == 'stimRunning':
-            self.exprmt.cleanup()
+        if self.logger.get_setup_state() == 'stimRunning':
+            self.exprmt.cleanup()  # close the window and cleanup after the protocol run
             self.logger.update_setup_state('sessionRunning')
 
     def do_stop_session(self):
-        self.do_stop_stim()
-        self.logger.update_setup_state('systemReady')
+        if self.logger.get_setup_state() == 'sessionRunning' or self.logger.get_setup_state() == 'stimRunning':
+            self.do_stop_stim()  # first stop the stimulaton
+            self.logger.update_setup_state('systemReady')
 
     def process_command(self,command):
+        if not command == self.prev_command: # only process changes in command
         #   process command
-        if command == 'Initialize':
-            # wait for initialization
-            self.do_initialize()
-        elif command == 'startSession':
-            self.do_start_session()
-        elif command == 'startStim':
-            self.do_start_stim()  # this is a busy loop
-        elif command == 'stopStim':
-            self.do_stop_stim()
-        elif command == 'stopSession':
-            self.do_stop_session()
-        else:
-            pass
+            self.prev_command = command
+            if command == 'Initialize':
+                # wait for initialization
+                self.do_initialize()
+            elif command == 'startSession':
+                self.do_start_session()
+            elif command == 'startStim':
+                self.do_start_stim()  # this is a busy loop
+            elif command == 'stopStim':
+                self.do_stop_stim()
+            elif command == 'stopSession':
+                self.do_stop_session()
+            else:
+                pass
