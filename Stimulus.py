fb5706849cff5c5ea55573ac4662d41f71e481d9
manolis <olakiril@hotmail.com>
2019-04-25 13:30:43 -0500

    Square wave gratings

--- Stimulus.py
+++ Stimulus.py
@@ -28,8 +28,8 @@ class Stimulus:
         # setup pygame
         pygame.init()
         #self.screen = pygame.display.set_mode(self.size)
-        self.screen = pygame.display.set_mode(self.size, NOFRAME | HWSURFACE | DOUBLEBUF | RESIZABLE)
-        #self.screen = pygame.display.set_mode(self.size, HWSURFACE)
+        #self.screen = pygame.display.set_mode(self.size, NOFRAME | HWSURFACE | DOUBLEBUF | RESIZABLE)
+        self.screen = pygame.display.set_mode(self.size, HWSURFACE | DOUBLEBUF)
         self.unshow()
         pygame.mouse.set_visible(0)
         pygame.display.toggle_fullscreen()
